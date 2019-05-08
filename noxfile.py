19e3c56c0cf8015bf1dc2d8f63b94d066b44a36f
Thomas Johnson <tomej2000@gmail.com>
2019-05-08 10:33:01 -0700

    issue-12: finish basic noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,20 +1,21 @@
 import nox
 
 
-# @nox.session(python=["3.7"])
-# def test(session):
-#     session.install("pytest")
-#     session.run("pytest")
+@nox.session(python=["3.7"], reuse_venv=True)
+def test(session):
+    session.install("pytest", "pytest-cov")
+    session.install("-e", ".")
+    session.run("pytest", "--cov=fables")
 
 
-@nox.session(python=["3.7"])
+@nox.session(python=["3.7"], reuse_venv=True)
 def lint(session):
     session.install("flake8")
     session.run("flake8", "fables")
     session.run("flake8", "tests")
 
 
-@nox.session(python=["3.7"])
+@nox.session(python=["3.7"], reuse_venv=True)
 def type_check(session):
     session.install("mypy")
     session.run("mypy", "--strict", "fables")
