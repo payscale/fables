76ea145da177f246c105d88c9d31d6a92512e5b7
Thomas Johnson <tomej2000@gmail.com>
2019-10-14 16:37:09 -0700

    issue-36: add python 3.8.0 and 3.8.1 to noxfile and CI

--- noxfile.py
+++ noxfile.py
@@ -1,27 +1,30 @@
 import nox
 
 
-@nox.session(python=["3.7.4"], reuse_venv=True)
+py_versions = ["3.7.4", "3.8.0", "3.8.1"]
+
+
+@nox.session(python=py_versions, reuse_venv=True)
 def blacken(session):
     session.install("black")
     session.run("black", "fables", "tests", "noxfile.py", "setup.py")
 
 
-@nox.session(python=["3.7.4"], reuse_venv=True)
+@nox.session(python=py_versions, reuse_venv=True)
 def test(session):
     session.install("pytest", "pytest-cov")
     session.install("-e", ".")
     session.run("pytest", "--cov=fables")
 
 
-@nox.session(python=["3.7.4"], reuse_venv=True)
+@nox.session(python=py_versions, reuse_venv=True)
 def lint(session):
     session.install("flake8")
     session.run("flake8", "fables")
     session.run("flake8", "tests")
 
 
-@nox.session(python=["3.7.4"], reuse_venv=True)
+@nox.session(python=py_versions, reuse_venv=True)
 def type_check(session):
     session.install("mypy")
     session.run("mypy", "--strict", "fables")
